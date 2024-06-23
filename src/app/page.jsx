3e20e8b3608c11a77897149c09660310cc4ec788
Roshish Dahal <dahalroshish2@gmail.com>
2024-06-23 15:47:16 +0530

    chore: Update global CSS and layout for responsive design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
-  return <div>
-   <Link href="/">Hello World</Link>
-  </div>;
+  return (
+     <div>
+      <Navbar/>
+      <Footer/> 
+    </div>
+  );
 }
